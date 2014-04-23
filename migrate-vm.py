854db251dd1055eafdabee689298c1188c3cb6e7
Sijis Aviles <sijis.aviles+github@gmail.com>
2014-04-23 16:04:01 -0500

    adding migration script

---
+++ migrate-vm.py
@@ -0,0 +1,34 @@
+from pyVmomi import vim
+from pyVim.connect import SmartConnect, Disconnect
+import getpass
+import vmutils
+import random
+
+si = None
+username = raw_input('Username: ')
+password = getpass.getpass(prompt='Password: ')
+vcenter = raw_input('vcenter: ')
+vm_name = raw_input('VM: ')
+esx_host = raw_input('ESX Host: ')
+
+try:
+    si = SmartConnect(host=vcenter, user=username, pwd=password, port=443)
+except IOError, e:
+    pass
+
+if esx_host == '':
+    all_hosts = vmutils.get_hosts(si)
+    esx_host = vmutils.get_host_by_name(si, random.choice(all_hosts.values()))
+
+# Finding source VM
+vm = vmutils.get_vm_by_name(si, vm_name)
+
+# relocate spec, to migrate to another host
+# this can do other things, like storage and resource pool
+# migrations
+relocate_spec = vim.vm.RelocateSpec(host=esx_host)
+
+# does the actual migration to host
+vm.Relocate(relocate_spec)
+
+Disconnect(si)
